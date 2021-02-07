ff646ab1676b35c2f87e061149cc27a26eb1a9a4
EC2 Default User <ec2-user@ip-172-31-30-45.ec2.internal>
2021-02-07 20:41:46 +0000

    Added CSS and link to preview album if song preview is unavailable

--- main.py
+++ main.py
@@ -19,19 +19,21 @@ headers = {
 params={ 'market': 'US'}
 artistID=['3TVXtAsR1Inumwj472S9r4', '6eUKZXaKkcviH0Ku9w2n3V', '66CXWjxzNUsdJxJ2JdwvnR', '4xRYI6VqpkE3UwrDrAZL8L','4MCBfE4596Uoi2O4DtmEMz','1RyvyyTE3xzB2ZywiAwp0i','07YZf4WDAMNwqr4jfgOZ8y','0C8ZW7ezQVs4URX5aX7Kqx']
 #Artists: Drake, Ed Sheeran, Ariana Grande, Logic, Juice Wrld, Future, Jason Derulo, Selena Gomez
-
 @app.route('/')
 def songInfo():
     rand_Artist = random.randint(0,7) #picks random artist from list with index 0-7
     BASE_URL = 'https://api.spotify.com/v1/artists/' #Base URL for get request
-    req = requests.get(BASE_URL+artistID[rand_Artist]+'/top-tracks', headers=headers, params=params).json() #GET request
+    req = requests.get(BASE_URL+artistID[rand_Artist]+'/top-tracks', headers=headers, params=params).json() #GET request for top tracks
     rand_song = random.randint(0,len(req['tracks'])-1) #picks random song from all the songs by certain artist
+    albumID=req['tracks'][rand_song]['album']['id']
+    albumReq=requests.get('https://api.spotify.com/v1/albums/'+albumID, headers=headers, params=params).json() #GET request for album
     global artistList
     global artistName
     artistList=req['tracks'][rand_song]['artists']
     songName=req['tracks'][rand_song]['name']
     songImage=req['tracks'][rand_song]['album']['images'][1]['url']
     songURL=req['tracks'][rand_song]['preview_url']
+    albumLink=albumReq['external_urls']['spotify']
     for i in range(len(artistList)): #range(len) prints indexes
         if (artistList[i]['id']==artistID[rand_Artist]): 
             artistName = req['tracks'][rand_song]['artists'][i]['name']
@@ -41,7 +43,8 @@ def songInfo():
         artistName=artistName,
         songName=songName,
         songImage=songImage,
-        songURL=songURL
+        songURL=songURL,
+        albumLink=albumLink
         )
 app.run(
     port=int(os.getenv('PORT',8080)), 
